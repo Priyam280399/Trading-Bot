a0abab639cf76470d135a5fa705a3586f75f7a57
Priyam Kumar Maurya <gudun28032000@gmail.com>
2024-10-05 00:10:46 +0530

    Resolved merge conflicts

--- stockPriceSimulator.js
+++ stockPriceSimulator.js
@@ -1,5 +1,7 @@
+// stockPriceSimulator.js
 function getMockStockPrice() {
     return (Math.random() * 100).toFixed(2); // returns a price between 0 and 100
   }
   
   module.exports = { getMockStockPrice };
+  
